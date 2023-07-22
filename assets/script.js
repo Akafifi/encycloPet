61fa40d7f760fe9326462f765ec8db0b906a6f3a
Akafifi <itsmeafifi@gmail.com>
2023-07-22 11:36:28 -0500

    started js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var catFactBtn = document.getElementById('cat-fact');
+var dogFactbtn = document.getElementById('dog-fact');
+var breedInputEl = document.querySelector('.search-breed');
+var breedForm = document.querySelector('breed-input');
+var petType = document.querySelector('#pet-select');
+var selectedPetType;
+var notification = document.querySelector('#modal');
+var notificationBtn = document.querySelector('.notification .delete');
+var clearEl = document.querySelector('#clear');
+var historyElDog = document.querySelector('search-history-dog');
+
